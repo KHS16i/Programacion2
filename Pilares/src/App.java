ff8d96a4da620f505b325af67cb92db9db104a9d
KHS16 <khs1694@hotmail.com>
2021-09-24 13:53:42 -0600

    Subiendo cambios

--- Pilares/src/App.java
+++ Pilares/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        Persona estudiante = new Persona();
+        estudiante.nombre = "Kevin";
+        estudiante.apellido = "Hernandez";
+        estudiante.id = 11568;
+        estudiante.edad = 27;
     }
 }
